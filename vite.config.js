fe78914b628434b249560c256f47013a1cc5c280
Anshuman Panigrahi <adrianlouis535@gmail.com>
2026-09-13 23:28:52 +0530

    first commit

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import { resolve, dirname } from "path"
+import { fileURLToPath } from "url"
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '#components': resolve(__dirname, 'src/components'),
+      '#constants': resolve(__dirname, 'src/constants'),
+      '#store': resolve(__dirname, 'src/store'),
+      '#hoc': resolve(__dirname, 'src/hoc'),
+      '#windows': resolve(__dirname, 'src/windows'),
+    }
+  }
+})
