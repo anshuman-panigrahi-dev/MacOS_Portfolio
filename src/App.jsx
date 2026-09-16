415fff72959a1c104d126cc9539f78997643cc7d
Anshuman Panigrahi <panigrahianshuman232@gmail.com>
2026-09-16 19:57:23 +0530

    implement dock presentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
+// import Navbar from '#components/Navbar'
+// import React from 'react'
+// import Welcome from '#components/Welcome'
+// import Dock from '#components/Dock'
+
+// const App = () => {
+//   return (
+// <main>
+//   <Navbar/>
+//   <Welcome />
+//   <Dock />
+// </main>
+//   )
+// }
+
+// export default App
 import Navbar from '#components/Navbar'
 import React from 'react'
+import Welcome from '#components/Welcome'
+import Dock from '#components/Dock'
+import { dockApps } from '#constants' // Import dockApps array here
 
 const App = () => {
   return (
-<main>
-  <Navbar/>
-</main>
+    <main>
+      <Navbar />
+      <Welcome />
+      <Dock dockApps={dockApps} /> {/* Pass dockApps as a prop */}
+    </main>
   )
 }
 
